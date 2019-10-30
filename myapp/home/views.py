d172d949642357bb37a74c31f8f09bcfd1971cb5
김민수 <alstn2468_@naver.com>
2019-10-30 22:00:17 +0900

    Fix views.py Item model import path

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from ..item.models import Item
+from myapp.item.models import Item
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
